9cb0ca4a5e796830cca191272bc34b9d66386a6c
freethewhat <mcsutton@outlook.com>
2017-09-01 14:23:26 -0600

    added newurl view

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const dbUrl = 'localhost:27017/urlshortener';
 const db = monk(dbUrl);
 
 db.then(() => {
-  console.log("Connected to Database");
+  console.log('Connected to Database');
 });
 
 /* GET home page. */
@@ -16,20 +16,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/addurl", function(req,res,next){
-  const countCollection = db.get("count");
-  const urlsCollection = db.get("urls");
+router.post('/addurl', function(req,res,next){
+  const countCollection = db.get('count');
+  const urlsCollection = db.get('urls');
+
+  let id = '';
+  let encryptedId = '';
 
   countCollection.findOne({}).then((doc) => {
-    let id = doc.count + 1;
-    let encryptedId = base58.encode(id);
-    urlsCollection.insert({"_id":id,"long_url":req.body.long_url,"date_creation":Date()});
-    countCollection.update({"count":id -1},{"count": id});
+    id = doc.count + 1;
+    encryptedId = base58.encode(id);
 
+    urlsCollection.insert({'_id':id,'long_url':req.body.long_url,'date_creation':Date()});
+    countCollection.update({'count':id -1},{'count': id});
+  }).then(() =>{
+    res.redirect('/newurl?short_url=' + encryptedId);
   });
-  //urlsCollection.insert({"_id":10001,"long_url":"written"});
+});
 
-
-  res.redirect('/');
+router.get('/newurl', function(req, res, next) {
+  res.render('newurl', {title: 'Your URL', 'short_url':req.param('short_url')});
 })
+
 module.exports = router;

2c79209fc98eeedfb39f3f8a8c30c3149821df37
freethewhat <mcsutton@outlook.com>
2017-09-01 14:40:23 -0600

    added dynamic router to index.js

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,7 @@ db.then(() => {
   console.log('Connected to Database');
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 
 router.post('/addurl', function(req,res,next){
   const countCollection = db.get('count');
@@ -22,6 +19,7 @@ router.post('/addurl', function(req,res,next){
 
   let id = '';
   let encryptedId = '';
+  let hostname = "http://localhost:3000/"
 
   countCollection.findOne({}).then((doc) => {
     id = doc.count + 1;
@@ -30,12 +28,31 @@ router.post('/addurl', function(req,res,next){
     urlsCollection.insert({'_id':id,'long_url':req.body.long_url,'date_creation':Date()});
     countCollection.update({'count':id -1},{'count': id});
   }).then(() =>{
-    res.redirect('/newurl?short_url=' + encryptedId);
+    res.redirect('/newurl?short_url=' + hostname + encryptedId);
   });
 });
 
 router.get('/newurl', function(req, res, next) {
   res.render('newurl', {title: 'Your URL', 'short_url':req.param('short_url')});
-})
+});
+
+router.get('/:encoded_id', function(req, res, next) {
+  urlsCollection = db.get('urls');
+  var decoded_id = base58.decode(req.param('encoded_id'));
+  console.log(decoded_id);
+
+  urlsCollection.findOne({"_id":decoded_id}).then((doc) => {
+    if(doc){
+      res.redirect(doc.long_url);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;

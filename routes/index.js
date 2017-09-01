863f8826c67f75be57f037d157a5db2d66f12f19
freethewhat <mcsutton@outlook.com>
2017-09-01 12:05:34 -0600

    added database connection to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var monk = require('monk');
+var base58 = require('base-58');
+
+const dbUrl = 'localhost:27017/urlshortener';
+const db = monk(dbUrl);
+
+db.then((err) => {
+  if(err){
+    console.log(err);
+  }
+  console.log("Connected to Database");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+
   res.render('index', { title: 'Express' });
 });
 
+router.post("/addurl", function(req,res,next){
+  res.redirect('addurl', '/');
+})
 module.exports = router;

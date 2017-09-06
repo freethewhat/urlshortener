498a4e4ce5cedaf3cd1a9cdada8ce4344cef27b9
freethewhat <mcsutton@outlook.com>
2017-09-06 07:14:34 -0600

    fixed and issue with block content

--- public/javascripts/urlshortener.js
+++ public/javascripts/urlshortener.js
@@ -1,5 +1,5 @@
 function validateForm(url){
-  let pattern = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+  let pattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
   let res = pattern.test(url);
 
   return res;
